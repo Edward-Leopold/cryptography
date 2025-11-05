c7860339c11c10e2689be8d4dc9ba2c8a0f4130c
Daniil Makhov <mahovdnl@gmail.com>
2025-11-05 18:48:36 +0300

    Tests added

---
+++ Tests/Utilities/PBlockTests.cs
@@ -0,0 +1,57 @@
+using cryptography.Utilities;
+
+namespace Tests.Utilities;
+
+public class PBlockTests
+{
+    [Theory]
+    [InlineData(
+        new byte[] { 0b11111110, 0b11111111, 0b11111111, 0b11111111 },
+        new byte[] { 0b01111111, 0b11111111, 0b11111111, 0b11111111 }
+    )]
+    public void PBlock_LowToHigh_Tests(
+        byte[] input, byte[] expected)
+    {
+        int[] pBlock =
+        [
+            16, 7, 20, 21,
+            29, 12, 28, 17,
+            1, 15, 23, 26,
+            5, 18, 31, 10,
+            2, 8, 24, 14,
+            32, 27, 3, 9,
+            19, 13, 30, 6,
+            22, 11, 4, 25,
+        ];
+
+        byte[] output = PBlock.Permutation(input, pBlock, PBlock.BitsIndexingMode.LowToHigh, 1);
+
+        // Assert
+        Assert.Equal(expected, output);
+    }
+
+    [Theory]
+    [InlineData(
+        new byte[] {0b11111111, 0b11111111, 0b11111111, 0b01111111 },
+new byte[] { 0b11111111, 0b11111111, 0b11111111, 0b11111110 }
+    )]
+    public void PBlock_HighToLow_Tests(
+        byte[] input, byte[] expected)
+    {
+        int[] pBlock = [
+            16, 7, 20, 21,
+            29, 12, 28, 17,
+            1, 15, 23, 26, 
+            5, 18, 31, 10,
+            2, 8, 24, 14, 
+            32, 27, 3, 9,
+            19, 13, 30, 6,
+            22, 11, 4, 25,
+        ]; 
+    
+        byte[] output = PBlock.Permutation(input, pBlock, PBlock.BitsIndexingMode.HighToLow, 1);
+    
+        // Assert
+        Assert.Equal(expected, output);
+    }
+}
