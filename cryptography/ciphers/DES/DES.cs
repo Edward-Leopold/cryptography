ab40cbf7bf79a453c2cc6f309aed6d5b7a254079
Daniil Makhov <mahovdnl@gmail.com>
2025-11-14 21:16:04 +0300

    DES done

--- cryptography/ciphers/DES/DES.cs
+++ cryptography/ciphers/DES/DES.cs
@@ -1,6 +1,41 @@
+using cryptography.Utilities;
+using cryptography.ciphers.DES.constants;
+using cryptography.feistel;
+using cryptography.ciphers.DES.components;
+using cryptography.interfaces;
+
 namespace cryptography.ciphers.DES;
 
-public class DES
+public class DES : ISymmetricEncryption
 {
+    private readonly FeistelNetwork _network;
+    private byte[]? _key;
+    public DES()
+    {
+        _network = new FeistelNetwork(new KeysGenerator(), new RoundEncryption());
+    }
     
+    public void SetKey(byte[] key)
+    {
+        _key = key ?? throw new ArgumentNullException(nameof(key));
+    }
+    public byte[] Encrypt(byte[] data)    
+    {
+        if (_key == null) throw new InvalidOperationException("Key not set");
+        var permuted = Permutations.PermutateByTable(data, DesConstants.IP, Permutations.BitsIndexingMode.HighToLow, 1);
+        var encrypted = _network.Encrypt(permuted, _key);
+        var inversionPermuted =
+            Permutations.PermutateByTable(encrypted, DesConstants.IP_INV, Permutations.BitsIndexingMode.HighToLow, 1);
+        return inversionPermuted;
+    }
+    
+    public byte[] Decrypt(byte[] data)
+    {
+        if (_key == null) throw new InvalidOperationException("Key not set");
+        var permuted = Permutations.PermutateByTable(data, DesConstants.IP, Permutations.BitsIndexingMode.HighToLow, 1);
+        var decrypted = _network.Encrypt(permuted, _key);
+        var inversionPermuted =
+            Permutations.PermutateByTable(decrypted, DesConstants.IP_INV, Permutations.BitsIndexingMode.HighToLow, 1);
+        return inversionPermuted;
+    }
 }
--- cryptography/interfaces/ISymmetricEncryption.cs
+++ cryptography/interfaces/ISymmetricEncryption.cs
@@ -3,7 +3,14 @@ namespace cryptography.interfaces;
 public interface ISymmetricEncryption
 { 
     // void SetRoundKeys(in byte[][] roundKeys); //IReadOnlyList<IReadOnlyList<byte>>(???)
+    // byte[] Encrypt(in byte[] inputBlock)
+    // byte[] Decrypt(in byte[] inputBlock)
     // redundunt method
+    
+    // byte[] Encrypt(in byte[] inputBlock, in byte[] key);
+    // byte[] Decrypt(in byte[] inputBlock, in byte[] key);
+    
+    void SetKey(byte[] key);
     byte[] Encrypt(byte[] inputBlock);
     byte[] Decrypt(byte[] inputBlock);
 }   
